e8c2da02062e3e08427d9ef9e4309f8e646d171e
DESKTOP-N6DF3V7\kys <ysookim@puzzleone.com>
2023-11-03 11:58:32 +0900

    Component용 네트워크 로그 매크로 추가

--- ArenaBattleSample/Source/ArenaBattleSample/ArenaBattleSample.h
+++ ArenaBattleSample/Source/ArenaBattleSample/ArenaBattleSample.h
@@ -6,7 +6,13 @@
 
 #define LOG_LOCAL_ROLE_INFO *(UEnum::GetValueAsString(TEXT("Engine.ENetRole"), GetLocalRole()))
 #define LOG_REMOTE_ROLE_INFO *(UEnum::GetValueAsString(TEXT("Engine.ENetRole"), GetRemoteRole()))
-#define LOG_NET_MODE_INFO (GetNetMode() == ENetMode::NM_Client ? *FString::Printf(TEXT("CLIENT_%d"), GPlayInEditorID) : GetNetMode() == ENetMode::NM_Standalone ? TEXT("STANDALONE") : TEXT("SERVER"))  
+
+#define LOG_SUB_LOCAL_ROLE_INFO *(UEnum::GetValueAsString(TEXT("Engine.ENetRole"), GetOwner()->GetLocalRole()))
+#define LOG_SUB_REMOTE_ROLE_INFO *(UEnum::GetValueAsString(TEXT("Engine.ENetRole"), GetOwner()->GetRemoteRole()))
+
+#define LOG_NET_MODE_INFO (GetNetMode() == ENetMode::NM_Client ? *FString::Printf(TEXT("CLIENT_%d"), GPlayInEditorID) : GetNetMode() == ENetMode::NM_Standalone ? TEXT("STANDALONE") : TEXT("SERVER"))
+
 #define AB_LOG(LogCat, Verbosity, Format, ...) UE_LOG(LogCat, Verbosity, TEXT("[%s][%s/%s] %s %s"), LOG_NET_MODE_INFO, LOG_LOCAL_ROLE_INFO, LOG_REMOTE_ROLE_INFO, *FString(__FUNCTION__), *FString::Printf(Format, ##__VA_ARGS__))
+#define AB_SUB_LOG(LogCat, Verbosity, Format, ...) UE_LOG(LogCat, Verbosity, TEXT("[%s][%s/%s] %s %s"), LOG_NET_MODE_INFO, LOG_SUB_LOCAL_ROLE_INFO, LOG_SUB_REMOTE_ROLE_INFO, *FString(__FUNCTION__), *FString::Printf(Format, ##__VA_ARGS__))
 
 DECLARE_LOG_CATEGORY_EXTERN(LogABNetwork, Log, All);
